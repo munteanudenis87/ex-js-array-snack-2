668fd0be4e04a303bb51d573e83195c838824152
munteanudenis87 <munteanudenis87@gmail.com>
2026-07-11 11:33:45 +0200

    first commit

---
+++ Snack 1/script.js
@@ -0,0 +1,59 @@
+// Crea una funzione che somma due numeri.
+//  Crea un array (longBooks) con i libri che hanno più di 300 pagine;
+//  Creare un array (longBooksTitles) che contiene solo i titoli dei libri contenuti in longBooks.
+//  Stampa in console ogni titolo nella console.
+
+const books = [
+  { 
+	  title: "React Billionaire", 
+	  pages: 250, 
+	  author: {
+		  name: 'Alice',
+		  age: 35
+	  },
+	  available: false,
+	  price: '101€',
+	  tags: ['advanced', 'js', 'react', 'senior']
+  },
+  { 
+	  title: "Advanced JS", 
+	  pages: 500, 
+	  author: {
+		  name: 'Bob',
+		  age: 20
+	  },
+	  available: true,
+	  price: '25€',
+	  tags: ['advanced', 'js', 'mid-senior']
+  },
+  { 
+	  title: "CSS Secrets", 
+	  pages: 320, 
+	  author: {
+		  name: 'Alice',
+		  age: 17
+	  },
+	  available: true,
+	  price: '8€',
+	  tags: ['html', 'css', 'junior']
+  },
+  { 
+	  title: "HTML Mastery", 
+	  pages: 200, 
+	  author: {
+		  name: 'Charlie',
+		  age: 50
+	  },
+	  available: false,
+	  price: '48€',
+	  tags: ['html', 'advanced', 'junior', 'mid-senior']
+  },
+];
+
+const longBooks = books.filter((book) => book.pages > 300);
+console.log(longBooks);
+
+const longBooksTitles = longBooks.map((book) => book.title);
+console.log(longBooksTitles);
+
+longBooksTitles.forEach(titolo => console.log(titolo));
